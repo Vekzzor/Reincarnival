38dc502e505424f3d123ebd5dfc9a455b7589127
Viktor Eklund <vikzzan@gmail.com>
2020-08-10 13:48:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# SpaceInvaders_FDS
+A code test for a job application. The objective was to create a space invaders game.
+
+Developed with Unreal Engine 4
---
+++ Source/SpaceInvaders_FDS/SpaceInvaders_FDS.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SpaceInvaders_FDS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SpaceInvaders_FDS, "SpaceInvaders_FDS" );
